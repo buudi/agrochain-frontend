cf74fbd24ef2c6c12edef42faeb865c03c2ec2b3
CatAteMaBurrito <63182939+CatAteMaBurrito@users.noreply.github.com>
2023-10-02 10:57:11 +0600

    Integrate deployed smart contract into frontend

--- src/ResultPage.js
+++ src/ResultPage.js
@@ -1,15 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { Box, Flex, Center, Text, Button, Progress, Badge, Skeleton } from "@chakra-ui/react";
 import { data } from "./data"; // Assuming the data array is in the same directory
+import { PeraWalletConnect } from '@perawallet/connect';
+import algosdk, { waitForConfirmation } from 'algosdk';
+
+
+const peraWallet = new PeraWalletConnect();
+
+const appIndex = 404238566;
+
+const algod = new algosdk.Algodv2('', 'https://testnet-api.algonode.cloud', 443);
 
 const ResultPage = () => {
   const [serialNumber, setSerialNumber] = useState("");
   const [product, setProduct] = useState(null);
 
+  const [accountAddress, setAccountAddress] = useState(null);
+  const isConnectedToPeraWallet = !!accountAddress;
+  const [retLocalVar, setRetLocalVar] = useState(null);
+  const [retGlobalVar, setRetGlobalVar] = useState(null);
+
   useEffect(() => {
+    checkGlobalVarState();
+
     const urlParams = new URLSearchParams(window.location.search);
     const term = urlParams.get("q");
     setSerialNumber(term);
+
+    peraWallet.reconnectSession().then((accounts) => {
+      // Setup disconnect event listener
+      peraWallet.connector?.on('disconnect', handleDisconnectWalletClick);
+
+      if (accounts.length) {
+        setAccountAddress(accounts[0]);
+      }
+    })
 
     // Find the product with the given serial number
     const foundProduct = data.find((item) => item.serial_number === parseInt(term));
@@ -34,10 +59,16 @@ const ResultPage = () => {
 
   return (
     <Flex flexDirection="column" alignItems="center" mt="8">
+      <Button className="btn-wallet"
+        onClick={
+          isConnectedToPeraWallet ? handleDisconnectWalletClick : handleConnectWalletClick
+        }>
+        {isConnectedToPeraWallet ? "Disconnect" : "Connect to Pera Wallet"}
+      </Button>
       {product ? (
         <>
           <Badge variant="solid" colorScheme="blue" p={2} fontSize="lg" mb="2">
-            Overall Rating: {calculateOverallRating()}
+            Overall Rating: {calculateOverallRating}
           </Badge>
           <Skeleton isLoaded={!!product.image_url}>
             <img src={product.image_url} alt={product.product_name} style={{ width: "300px", marginBottom: "20px" }} />
@@ -48,7 +79,7 @@ const ResultPage = () => {
           <Text fontSize="xl" mb="2">
             Serial Number: {serialNumber}
           </Text>
-          
+
           <Box w="100%" maxW="500px">
             {["Seed Storage", "Crop Growth", "Transportation Emission", "Processing and Packaging"].map((label) => (
               <Box key={label} mb="4">
@@ -76,6 +107,63 @@ const ResultPage = () => {
       )}
     </Flex>
   );
+
+  function handleConnectWalletClick() {
+    peraWallet.connect().then((newAccounts) => {
+      // setup the disconnect event listener
+      peraWallet.connector?.on('disconnect', handleDisconnectWalletClick);
+
+      setAccountAddress(newAccounts[0]);
+    });
+  }
+
+  function handleDisconnectWalletClick() {
+    peraWallet.disconnect();
+    setAccountAddress(null);
+  }
+
+  async function checkLocalVarState() {
+    try {
+      const accountInfo = await algod.accountApplicationInformation(accountAddress, appIndex).do();
+      if (!!accountInfo['app-local-state']['key-value'][0].value.uint) {
+        setRetLocalVar(accountInfo['app-local-state']['key-value'][0].value.uint);
+      } else {
+        setRetLocalVar(0);
+      }
+      console.log(accountInfo['app-local-state']['key-value'][0].value.uint);
+    } catch (e) {
+      console.error('There was an error connecting to the algorand node: ', e)
+    }
+  }
+
+  async function optInToApp() {
+    const suggestedParams = await algod.getTransactionParams().do();
+    const optInTxn = algosdk.makeApplicationOptInTxn(
+      accountAddress,
+      suggestedParams,
+      appIndex
+    );
+
+    const optInTxGroup = [{ txn: optInTxn, signers: [accountAddress] }];
+
+    const signedTx = await peraWallet.signTransaction([optInTxGroup]);
+    console.log(signedTx);
+    const { txId } = await algod.sendRawTransaction(signedTx).do();
+    const result = await waitForConfirmation(algod, txId, 2);
+  }
+
+  async function checkGlobalVarState() {
+    try {
+      const counter = await algod.getApplicationByID(appIndex).do();
+      if (!!counter.params['global-state'][0].value.uint) {
+        setRetGlobalVar(counter.params['global-state'][0].value.uint);
+      } else {
+        setRetGlobalVar(0);
+      }
+    } catch (e) {
+      console.error('There was an error connecting to the algorand node: ', e)
+    }
+  }
 };
 
 export default ResultPage;
